d95547b3ae4813d2f2d353889d74f5dd3bb96240
zizicici <1940699+zizicici@users.noreply.github.com>
2023-02-21 09:38:39 +0800

    Add a sample ClockHandRotationEffect.xcframework

---
+++ ClockHandRotationEffect.xcframework/ios-arm64/ClockHandRotationEffect.framework/Headers/ClockHandRotationEffect.h
@@ -0,0 +1,18 @@
+//
+//  ClockHandRotationEffect.h
+//  ClockHandRotationEffect
+//
+//  Created by Ci Zi on 2023/2/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ClockHandRotationEffect.
+FOUNDATION_EXPORT double ClockHandRotationEffectVersionNumber;
+
+//! Project version string for ClockHandRotationEffect.
+FOUNDATION_EXPORT const unsigned char ClockHandRotationEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClockHandRotationEffect/PublicHeader.h>
+
+
